ab658bba381bdee5168ec89b052af26873c9186e
achingbrain <alex@achingbrain.net>
2014-05-30 14:52:31 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,182 @@
+# nano-repository
+
+Working with CouchDB is a pretty pleasant experience, mostly because of the rather excellent [nano](https://github.com/dscape/nano) library.
+
+There are a couple of rough edges though, so what else could be better but an abstracton on top of an abstraction!
+
+## Examples
+
+Create me a repository:
+
+```javascript
+var Nano = require('nano'),
+  Repository = require('nano-repository');
+
+// set up Nano
+var connection = new Nano('http://localhost:5984');
+var db = connection.db.create('my_db');
+
+// set up repo
+var repository = new Repository(db);
+
+```
+
+### CRUD operations
+
+```javascript
+var document = {foo: 'bar'};
+
+// create
+repository.save(document, function(error, result) {
+    // document now has _id, _rev and created_at properties
+});
+
+// retrieve
+repository.findById(id, function(error, document) {
+    // document has been fetched
+});
+
+// update
+repository.save(document, function(error, result) {
+    // if previously saved, document now has updated_at properties
+    // and _rev has been incremented
+});
+
+// delete
+repository.remove(document, function(error) {
+    // document has been deleted
+});
+```
+
+### Attachments
+
+Add files to your documents:
+
+```javascript
+repository.addAttachment(document, attachmentName, pathToFile, mimeType, function(error, body) {
+  // file has now been attached
+});
+```
+
+Retrieve attachments:
+
+```javascript
+repository.findAttachment(document, name, function(error, body) {
+  // body contains the attachment data
+});
+```
+
+### Attachment versions
+
+The attachment version is dependent on the document version.  E.g if you will pardon the pyramid of doom:
+
+```javascript
+var attachmentName = 'myFile';
+
+// add the first version of the attachment
+repository.addAttachment(document, attachmentName, pathToFile, mimeType, function(error, body) {
+  // now document._rev = 2
+
+  // add the second it again:
+  repository.addAttachment(document, attachmentName, pathToFile, mimeType, function(error, body) {
+    // now document._rev = 3
+
+    // we now have two revisions of 'myFile' that we can access:
+    repository.findAttachment({_id: document._id, _rev: 2}, attachmentName, function(error, body) {
+      // body contains the first version of myFile
+    });
+    repository.findAttachment({_id: document._id, _rev: 3}, attachmentName, function(error, body) {
+      // body contains the second version
+    });
+  });
+});
+```
+
+#### Streaming attachments
+
+For the performance/memory conscious, stream files to your documents with the 4x argument version of Repository.addAttachment:
+
+```javascript
+var fileStream = fs.createReadStream(pathToFile);
+
+fileStream.pipe(repository.addAttachment(document, attachmentName, mimeType, function(error, body) {
+  // file has now been attached
+}));
+```
+
+...and retrieve them:
+
+```javascript
+repository.streamAttachmentTo(document, name, writeStream);
+```
+
+### Views
+
+Views are accessed by extra methods on the Repository.  To create your views, first create a view template file, usally with the `.json` extension.
+
+The template file looks like this (n.b. feel free to include `reduce` functions if you need them):
+
+```javascript
+{
+  "views": {
+    "all": {
+      "map": "function(doc) {if(doc.name) emit(null, doc)}"
+    },
+    "byName": {
+      "map": "function(doc) {if(doc.name) emit(doc.name, doc)}"
+    }
+  }
+}
+```
+
+Update/create the views:
+
+```javascript
+var repository = new Repository(db);
+repository.updateViews('path/to/views.json', function(error) {
+  // views are now ready to use
+});
+```
+
+View methods are dynamically created:
+
+```javascript
+repository.findAll(function(error, list) {
+  // list contains all documents from this collection
+});
+```
+
+Method names are chosen by capitalising the first letter of the view name and prepending `find` to it.
+
+So the view mapping file contained a view named `all` - this was turned into `findAll`, similarly the view named `byName` was turned into a method named `findByName`.
+
+Arguments are also supported:
+
+```javascript
+repository.findByName('bob', function(error, list) {
+  // list contains all documents where doc.name == 'bob'
+});
+```
+
+The code that makes up your view is hashed - the hash is stored along with the views.  The next time you call Repository.updateViews the hashes are compared - it they've changed then the views are recreated automatically.
+
+This means it's save to call Repository.updateViews every time you start your app - they'll only get altered if you change the code in your view file.
+
+## Get out of my console.log
+
+You can pass an alternative logging implementation into the constructor and the repository will use that instead, as long as it supports `.info`, `.warn`, etc methods.
+
+```javascript
+var Nano = require('nano'),
+  Logger = require('winston').Logger,
+  logger = new Logger(..);
+
+//.. set up Nano as usual
+
+var repository = new Repository(db, logger);
+```
+
+## Todo
+
+1. Retrieving specific document versions.
+3. Work out if I've missed the point somehow. Probably.
---
+++ test/RepositoryTest.js
@@ -0,0 +1,332 @@
+var Repository = require('../lib/Repository'),
+  sinon = require('sinon'),
+  chai = require('chai').should,
+  expect = require('chai').expect,
+  util = require('util'),
+  fs = require('fs');
+
+var TestRepository = function(db) {
+  Repository.call(this, db);
+};
+util.inherits(TestRepository, Repository);
+
+// stub nano object
+var db;
+
+describe('Repository', function() {
+  beforeEach(function() {
+    db = {
+      get: sinon.stub(),
+      config: {
+        db: 'test_database'
+      },
+      insert: sinon.stub(),
+      view: sinon.stub(),
+      destroy: sinon.stub(),
+      attachment: {
+        insert: sinon.stub(),
+        get: sinon.stub()
+      }
+    }
+  });
+
+  it('should object when no database passed', function() {
+    expect(function() {
+      new TestRepository();
+    }).to.throw;
+  });
+
+  it('should generate methods from views', function (done) {
+    db.get.withArgs('_design/test_database', sinon.match.func).callsArgWith(1, undefined, {});
+    db.insert.withArgs(sinon.match.object, '_design/test_database', sinon.match.func).callsArgWith(2, undefined);
+
+    var repository = new TestRepository(db);
+
+    // these methods will be defined by the view definition
+    expect(repository.findAll).to.not.be.a('function');
+    expect(repository.findByName).not.to.be.a('function');
+
+    // update the views
+    repository.updateViews(__dirname + '/fixtures/ViewDefinitions.json', function(error) {
+      if(error) {
+        fail(error);
+      }
+
+      // these methods are defined by the view definitions
+      expect(repository.findAll).to.be.a('function');
+      expect(repository.findByName).to.be.a('function');
+
+      done();
+    });
+  });
+
+  it('should call view methods', function (done) {
+    db.get.withArgs('_design/test_database', sinon.match.func).callsArgWith(1, undefined, {
+      hash: "47c7aeb99d395048244a1723f363d4c5"
+    });
+    db.view.withArgs('test_database', 'all', sinon.match.func).callsArgWith(2, undefined, {
+      rows: [{}, {}, {}]
+    });
+
+    var repository = new TestRepository(db);
+
+    // update the views
+    repository.updateViews(__dirname + '/fixtures/ViewDefinitions.json', function(error) {
+      if(error) {
+        fail(error);
+      }
+
+      repository.findAll(function(error, result) {
+        expect(result.length).to.equal(3);
+
+        done();
+      });
+    });
+  });
+
+  it('should call view methods with parameters', function (done) {
+    db.get.withArgs('_design/test_database', sinon.match.func).callsArgWith(1, undefined, {
+      hash: "47c7aeb99d395048244a1723f363d4c5"
+    });
+    db.view.withArgs('test_database', 'byName', {keys: ['foo']}, sinon.match.func).callsArgWith(3, undefined, {
+      rows: [{}, {}, {}, {}]
+    });
+
+    var repository = new TestRepository(db);
+
+    // update the views
+    repository.updateViews(__dirname + '/fixtures/ViewDefinitions.json', function(error) {
+      if(error) {
+        fail(error);
+      }
+
+      repository.findByName('foo', function(error, result) {
+        expect(result.length).to.equal(4);
+
+        done();
+      });
+    });
+  });
+
+  it('should not overwrite views when they have not changed', function (done) {
+    db.get.withArgs('_design/test_database', sinon.match.func).callsArgWith(1, undefined, {
+      hash: "47c7aeb99d395048244a1723f363d4c5"
+    });
+
+    var repository = new TestRepository(db);
+
+    // these methods will be defined by the view definition
+    expect(repository.findAll).to.not.be.a('function');
+    expect(repository.findByName).not.to.be.a('function');
+
+    // update the views
+    repository.updateViews(__dirname + '/fixtures/ViewDefinitions.json', function(error) {
+      if(error) {
+        fail(error);
+      }
+
+      sinon.assert.notCalled(db.insert);
+
+      done();
+    });
+  });
+
+  it('should overwrite views when they have not changed', function (done) {
+    db.get.withArgs('_design/test_database', sinon.match.func).callsArgWith(1, undefined, {
+      hash: "lalalala"
+    });
+    db.insert.withArgs(sinon.match.object, '_design/test_database', sinon.match.func).callsArgWith(2, undefined);
+
+    var repository = new TestRepository(db);
+
+    // these methods will be defined by the view definition
+    expect(repository.findAll).to.not.be.a('function');
+    expect(repository.findByName).not.to.be.a('function');
+
+    // update the views
+    repository.updateViews(__dirname + '/fixtures/ViewDefinitions.json', function(error) {
+      if(error) {
+        fail(error);
+      }
+
+      db.insert.withArgs('_design/test_database', sinon.match.func);
+
+      done();
+    });
+  });
+
+  it('should find by id', function (done) {
+    var id = 'foo';
+    var document = {
+      bar: 'baz'
+    };
+
+    db.get.withArgs(id, sinon.match.func).callsArgWith(1, undefined, document);
+
+    var repository = new TestRepository(db);
+    repository.findById(id, function(error, result) {
+      expect(result).to.equal(document);
+
+      done();
+    });
+  });
+
+  it('should save', function (done) {
+    var id = 'foo';
+    var rev = 1;
+    var document = {
+      bar: 'baz'
+    };
+
+    db.insert.withArgs(document, sinon.match.func).callsArgWith(1, undefined, {
+      id: id,
+      rev: rev
+    });
+
+    var repository = new TestRepository(db);
+    repository.save(document, function(error, result) {
+      expect(document._id).to.equal(id);
+      expect(document._rev).to.equal(rev);
+      expect(document.created_at).to.be.ok;
+
+      done();
+    });
+  });
+
+  it('should update', function (done) {
+    var id = 'foo';
+    var rev = 2;
+    var document = {
+      bar: 'baz',
+      created_at: new Date()
+    };
+
+    db.insert.withArgs(document, sinon.match.func).callsArgWith(1, undefined, {
+      id: id,
+      rev: rev
+    });
+
+    var repository = new TestRepository(db);
+    repository.save(document, function(error, result) {
+      expect(document._id).to.equal(id);
+      expect(document._rev).to.equal(rev);
+      expect(document.updated_at).to.be.ok;
+
+      done();
+    });
+  });
+
+  it('should remove', function (done) {
+    var id = 'foo';
+    var document = {
+      bar: 'baz',
+      created_at: new Date(),
+      _rev: 2,
+      _id: id
+    };
+
+    db.destroy.withArgs(id, document._rev, sinon.match.func).callsArg(2);
+
+    var repository = new TestRepository(db);
+    repository.remove(document, function() {
+      done();
+    });
+  });
+
+  it('should add an attachment', function (done) {
+    var document = {
+      bar: 'baz',
+      created_at: new Date(),
+      _id: 'foo',
+      _rev: 2
+    };
+    var name = 'my file';
+    var mimeType = 'text/plain';
+    var file = __dirname + '/fixtures/file.txt';
+    var body = {
+      rev: 3
+    };
+
+    db.attachment.insert.withArgs(document._id, name, sinon.match.object, mimeType, sinon.match.object, sinon.match.func).callsArgWith(5, undefined, body);
+
+    var repository = new TestRepository(db);
+    repository.addAttachment(document, name, file, mimeType, function(error, body) {
+      expect(document._rev).to.equal(body.rev);
+
+      done();
+    });
+  });
+
+  it('should receive a streamed attachment', function (done) {
+    var document = {
+      bar: 'baz',
+      created_at: new Date(),
+      _id: 'foo',
+      _rev: 2
+    };
+    var name = 'my file';
+    var mimeType = 'text/plain';
+    var fileStream = fs.createReadStream(__dirname + '/fixtures/file.txt');
+    var body = {
+      rev: 3
+    };
+
+    db.attachment.insert.withArgs(document._id, name, null, mimeType, sinon.match.object, sinon.match.func).returns({
+      on: sinon.stub(),
+      once: sinon.stub(),
+      emit: sinon.stub(),
+      write: sinon.stub(),
+      end: sinon.stub()
+    }).callsArgWith(5, undefined, body);
+
+    var repository = new TestRepository(db);
+
+    fileStream.pipe(repository.addAttachment(document, name, mimeType, function(error, body) {
+      expect(document._rev).to.equal(body.rev);
+
+      done();
+    }));
+  });
+
+  it('should retrieve an attachment', function (done) {
+    var document = {
+      bar: 'baz',
+      created_at: new Date(),
+      _id: 'foo',
+      _rev: 2
+    };
+    var name = 'my file';
+    var bytes = [0x00, 0x00, 0x00];
+
+    db.attachment.get.withArgs(document._id, name, sinon.match.func).callsArgWith(2, undefined, bytes);
+
+    var repository = new TestRepository(db);
+    repository.findAttachment(document, name, function(error, body) {
+      expect(body).to.equal(bytes);
+
+      done();
+    });
+  });
+
+  it('should stream an attachment', function() {
+    var document = {
+      bar: 'baz',
+      created_at: new Date(),
+      _id: 'foo',
+      _rev: 2
+    };
+    var name = 'my file';
+    var pipe = {
+      pipe: sinon.stub()
+    };
+    var output = {};
+
+    db.attachment.get.withArgs(document._id, name).returns(pipe);
+
+    var repository = new TestRepository(db);
+    repository.streamAttachmentTo(document, name, output);
+
+    // should have streamed to the passed output
+    pipe.pipe.calledWith(output);
+  });
+});
